eee6adb67613dd54c71b90447ef3057806b992fa
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2022-04-05 18:23:24 -0400

    chore: update ui colors

--- pages/auth/signup.tsx
+++ pages/auth/signup.tsx
@@ -27,7 +27,7 @@ const SignUp: NextPage = () => {
         <Link href="/">
           <a className="flex items-center">
             <Image src={'/favicon.ico'} alt="logo" width={20} height={20} />
-            <span className="ml-2 font-semibold">Live atc</span>
+            <span className="ml-2 font-semibold">Live ATC</span>
           </a>
         </Link>
       </div>
@@ -36,14 +36,14 @@ const SignUp: NextPage = () => {
           <div className="max-w-screen-2xl px-4 md:px-8 mx-auto">
             <h2 className="text-2xl lg:text-3xl font-bold text-center mb-4 md:mb-8">Sign Up</h2>
 
-            <div className="max-w-lg border border-gray-300 dark:border-gray-500 rounded-lg mx-auto">
+            <div className="max-w-lg border border-gray-300 dark:border-gray-600 rounded-lg mx-auto">
               <div className="flex flex-col gap-4 p-4 md:p-8">
                 <div>
                   <label htmlFor="email" className="inline-block text-sm sm:text-base mb-2">
                     Email
                   </label>
                   <input
-                    className="w-full border dark:border-none dark:bg-gray-600 focus:ring ring-brand-300 rounded-lg outline-none transition duration-100 px-3 py-2"
+                    className="w-full border dark:border-none dark:bg-gray-600 focus:ring ring-blue-400 rounded-lg outline-none transition duration-100 px-3 py-2"
                     name="email"
                     type="email"
                     value={email}
@@ -56,7 +56,7 @@ const SignUp: NextPage = () => {
                     Password
                   </label>
                   <input
-                    className="w-full border dark:border-none dark:bg-gray-600 focus:ring ring-brand-300 rounded-lg outline-none transition duration-100 px-3 py-2"
+                    className="w-full border dark:border-none dark:bg-gray-600 focus:ring ring-blue-400 rounded-lg outline-none transition duration-100 px-3 py-2"
                     name="password"
                     type="password"
                     value={password}
@@ -65,7 +65,7 @@ const SignUp: NextPage = () => {
                 </div>
 
                 <button
-                  className="block bg-brand-700 hover:bg-brand-800 active:bg-gray-600 text-white focus-visible:ring ring-gray-300 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3 mt-4"
+                  className="block bg-blue-500 hover:bg-blue-600 active:bg-gray-600 text-white focus-visible:ring ring-gray-300 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3 mt-4"
                   onClick={(e) => {
                     e.preventDefault()
                     signUpWithEmail(email, password)
@@ -98,42 +98,13 @@ const SignUp: NextPage = () => {
                   </svg>
                   <span>Sign Up with Github</span>
                 </button>
-
-                <button className="flex justify-center items-center border dark:border-none bg-white text-gray-800 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 gap-2 px-8 py-3">
-                  <svg
-                    className="w-5 h-5 flex-shrink-0"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M23.7449 12.27C23.7449 11.48 23.6749 10.73 23.5549 10H12.2549V14.51H18.7249C18.4349 15.99 17.5849 17.24 16.3249 18.09V21.09H20.1849C22.4449 19 23.7449 15.92 23.7449 12.27Z"
-                      fill="#4285F4"
-                    />
-                    <path
-                      d="M12.2549 24C15.4949 24 18.2049 22.92 20.1849 21.09L16.3249 18.09C15.2449 18.81 13.8749 19.25 12.2549 19.25C9.12492 19.25 6.47492 17.14 5.52492 14.29H1.54492V17.38C3.51492 21.3 7.56492 24 12.2549 24Z"
-                      fill="#34A853"
-                    />
-                    <path
-                      d="M5.52488 14.29C5.27488 13.57 5.14488 12.8 5.14488 12C5.14488 11.2 5.28488 10.43 5.52488 9.71V6.62H1.54488C0.724882 8.24 0.254883 10.06 0.254883 12C0.254883 13.94 0.724882 15.76 1.54488 17.38L5.52488 14.29Z"
-                      fill="#FBBC05"
-                    />
-                    <path
-                      d="M12.2549 4.75C14.0249 4.75 15.6049 5.36 16.8549 6.55L20.2749 3.13C18.2049 1.19 15.4949 0 12.2549 0C7.56492 0 3.51492 2.7 1.54492 6.62L5.52492 9.71C6.47492 6.86 9.12492 4.75 12.2549 4.75Z"
-                      fill="#EA4335"
-                    />
-                  </svg>
-                  <span>Sign Up with Google</span>
-                </button>
               </div>
 
               <div className="flex justify-center items-center pb-4">
                 <p className="text-sm text-center">
                   Have an account?
                   <Link href="/auth/signin">
-                    <a className="text-brand-600 hover:text-brand-700 active:text-brand-700 transition duration-100 ml-1">
+                    <a className="text-blue-400 hover:text-blue-500 active:text-blue-500 transition duration-100 ml-1">
                       Sign In
                     </a>
                   </Link>
