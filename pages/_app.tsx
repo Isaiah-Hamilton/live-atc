bcce3489a06ec232909ad71d610a83952cc42ab3
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2023-03-28 00:57:12 -0400

    fix font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,6 @@
 import '@/styles/globals.css'
-import { Inter } from 'next/font/google'
 import type { AppProps } from 'next/app'
 
-const inter = Inter({ subsets: ['latin'] })
-
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <div className={inter.className}>
-      <Component {...pageProps} />
-    </div>
-  )
+  return <Component {...pageProps} />
 }
