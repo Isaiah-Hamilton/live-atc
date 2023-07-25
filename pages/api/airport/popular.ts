c78e8d566788cbeb0e086e0de157f47628bdfdd1
Isaiah Hamilton <70828596+Isaiah-Hamilton@users.noreply.github.com>
2023-07-25 13:28:12 -0400

    saving for new nextjs site

---
+++ pages/api/airport/popular.ts
@@ -0,0 +1,16 @@
+import { NextApiResponse } from 'next'
+import supabase from '../../../lib/supabase'
+
+export default async (res: NextApiResponse) => {
+  const { data: airports, error } = await supabase
+    .from('airports')
+    .select('*')
+    .order('views', { ascending: false })
+    .limit(5)
+
+  if (error) {
+    return res.status(500).json({ error })
+  }
+
+  return res.status(200).json({ airports })
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,85 +1,66 @@
-import { NextPage } from 'next'
-import { useState, useEffect } from 'react'
+import type { NextPage } from 'next'
 import Link from 'next/link'
 import Image from 'next/image'
-import supabase, { SUPABASE_URL } from '../lib/supabase'
+// import { SUPABASE_URL } from '../lib/supabase'
 import Layout from '../components/Layout'
 import Section from '../components/Section'
 import Balancer from 'react-wrap-balancer'
+ 
+export const getServerSideProps = async ({ query }: any) => {
+  const { lat, lon } = query
 
-const Home: NextPage = () => {
-  const [featuredData, setFeaturedData] = useState<any>([])
-  const [popularData, setPopularData] = useState<any>([])
-  useEffect(() => {
-    const fetchFeaturedData = async () => {
-      let { data: airports } = await supabase
-        .from('airports')
-        .select('*')
-        .eq('icao', 'KATL')
-        .single()
-      // @ts-ignore
-      setFeaturedData(airports)
-    }
+  const featuredResponse = await fetch(`http://localhost:3000/api/featured-airport?lat=${lat}&lon=${lon}`)
+  const featuredData = await featuredResponse.json()
 
-    const fetchPopularData = async () => {
-      let { data: airports } = await supabase
-        .from('airports')
-        .select('*')
-        .order('views', { ascending: false })
-        .limit(5)
-      // @ts-ignore
-      setPopularData(airports)
-    }
+  //const popularResponse = await fetch(`http://localhost:3000/api/popular-airports`)
+  //const popularData = await popularResponse.json()
 
-    fetchFeaturedData()
-    fetchPopularData()
-  }, [])
+  return {
+    props: {
+      lat: parseFloat(lat),
+      lon: parseFloat(lon),
+      featuredAirport: featuredData.airport,
+      //popularAirports: popularData.airports,
+    },
+  }
+}
 
+const Home: NextPage = ({ featuredAirport, popularAirports }: any) => {
   return (
     <Layout>
-      {/* Featured */}
       <Section>
-        <Link href={`/airport/katl`}>
-          <div className="relative w-full h-96 overflow-hidden rounded-xl shadow-lg">
-            <Image
-              src={`${SUPABASE_URL}/storage/v1/object/public/airports/${featuredData?.icao}.jpg`}
-              alt="Airport Image"
-              layout="fill"
-              objectFit="cover"
-              className="w-full h-full brightness-50"
-            />
-            <div className="absolute bottom-0 inset-x-0 text-white text-2xl md:text-4xl font-medium px-4 py-2">
-              <Balancer>
-                {featuredData?.icao} - {featuredData?.full_name}
-              </Balancer>
-            </div>
+        <Link href={`/airport/${featuredAirport?.id}`}>
+          <div className="w-full rounded-xl shadow-lg">
+            <div className='text-4xl'>{featuredAirport?.id}</div>
+            <Balancer className='text-4xl'>
+              {featuredAirport?.name}
+            </Balancer>
+            <div className='text-4xl'>{featuredAirport?.city}, {featuredAirport?.region}</div>
           </div>
         </Link>
       </Section>
 
-      {/* Popular */}
       <Section>
         <h1 className="text-4xl font-semibold mb-4">Popular</h1>
         <div className="flex items-center space-x-4 py-8 justify-between overflow-x-scroll">
-          {popularData?.map((airport: any, i: number) => (
+          {/*popularAirports?.map((airport: any, i: number) => (
             <div className="hover:-translate-y-4 ease-in-out duration-300" key={i}>
-              <Link href={`/airport/${airport.icao}`}>
+              <Link href={`/airport/${airport.id}`}>
                 <div className="relative w-60 h-44 md:w-80 md:h-64 overflow-hidden rounded-xl shadow-md bg-gray-100 dark:bg-gray-700">
-                  <Image
-                    src={`${SUPABASE_URL}/storage/v1/object/public/airports/${airport.icao}.jpg`}
-                    alt="Airport Image"
-                    layout="fill"
-                    objectFit="cover"
-                    className="w-full h-full brightness-50"
-                  />
                   <div className="absolute bottom-0 inset-x-0 text-white text-4xl font-medium p-2">
-                    {airport.icao}
+                    <p>{airport.id}</p>
+                    <p>{airport.name}</p>
+                    <p>{featuredAirport?.city}, {featuredAirport?.region}</p>
                   </div>
                 </div>
               </Link>
             </div>
-          ))}
+          ))*/}
         </div>
+      </Section>
+
+      <Section>
+        <h1 className="text-4xl font-semibold mb-4">New Live ATC Feed</h1>
       </Section>
     </Layout>
   )
