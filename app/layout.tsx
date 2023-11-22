5af6f821acff2f9374862dcf282582b0ffc47d0c
Isaiah Hamilton <70828596+Isaiah-Hamilton@users.noreply.github.com>
2023-11-22 18:07:00 -0500

    init shadcn/ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { cn } from '@/lib/utils'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -12,7 +13,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn('min-h-screen bg-background font-sans antialiased', inter.className)}>
+        {children}
+      </body>
     </html>
   )
 }
