b6d8b7ae22b213b94f93fde4a321090bbaa36b4d
Isaiah Hamilton <70828596+Isaiah-Hamilton@users.noreply.github.com>
2023-11-22 19:35:23 -0500

    add hero section to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,25 @@
-'use client'
-
-import * as React from 'react'
-import { Moon, Sun } from 'lucide-react'
-import { useTheme } from 'next-themes'
-
-import { Button } from '@/components/ui/button'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
+import Link from 'next/link'
+import { buttonVariants } from '@/components/ui/button'
 
 const Home = () => {
-  const { setTheme } = useTheme()
-
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme('light')}>Light</DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('dark')}>Dark</DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('system')}>System</DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <div className="w-fit mx-auto space-y-6">
+      <div className="text-center space-y-1.5">
+        <div className="text-5xl lg:text-7xl font-semibold space-y-1 tracking-tighter">
+          <h1>Listen to Live</h1>
+          <h1>Air Traffic Control</h1>
+        </div>
+        <p className="text-xl">Listen up to 1,000+ live Frequencies for free</p>
+      </div>
+      <div className="w-fit mx-auto flex items-center space-x-4 font-medium">
+        <Link href="/airport" className={buttonVariants({ variant: 'default' })}>
+          Listen Now
+        </Link>
+        <Link href="/" className={buttonVariants({ variant: 'outline' })}>
+          Random Airport
+        </Link>
+      </div>
+    </div>
   )
 }
 
