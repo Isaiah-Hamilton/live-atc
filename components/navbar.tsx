757e087c150bad0fb3d153885eadedc44e47113d
Isaiah Hamilton <70828596+Isaiah-Hamilton@users.noreply.github.com>
2023-11-22 20:51:14 -0500

    update navbar input and theme toggle

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -23,7 +23,7 @@ const ThemeToggle = () => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button className="aspect-square" variant="outline" size="icon">
+        <Button className="aspect-square m-1" variant="outline" size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
@@ -76,11 +76,12 @@ const Navbar = () => {
               Airports
             </Link>
 
-            <div className="flex items-center gap-x-2 sm:ms-auto">
+            <div className="flex items-center sm:ms-auto">
               <Input
                 type="search"
                 value={search}
                 placeholder="Search by icao"
+                className="m-1"
                 onChange={(e) => handleInputChange(e)}
                 onKeyDown={(e) => handleInputEnter(e)}
               />
