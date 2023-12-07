66aced38389a0bb99b2c66aa58f9a7f362840443
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2023-12-06 21:57:59 -0500

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 import { Database } from './database.type'
 
-const SUPABASE_URL = process.env.SUPABASE_URL || 'https://ecymmfjqjfxjgydzysng.supabase.co'
-const SUPABASE_KEY = process.env.SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVjeW1tZmpxamZ4amd5ZHp5c25nIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDkwMDk1MjcsImV4cCI6MTk2NDU4NTUyN30.mN2D5Yv5Bo3T9lBcNCSR4v_wdj5tGzBc_KblpNV8sRs'
+const SUPABASE_URL = process.env.SUPABASE_URL!
+const SUPABASE_KEY = process.env.SUPABASE_KEY!
 
 const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_KEY)
 
